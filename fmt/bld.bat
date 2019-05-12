711f30e5aff4d665199454594b9dc14245607067
Hisanari Otsu <hi2p.perim@gmail.com>
2019-05-12 21:50:55 +0200

    added fmt

---
+++ fmt/bld.bat
@@ -0,0 +1,10 @@
+cmake -H. -B_build ^
+      -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D FMT_DOC=OFF ^
+      -D FMT_TEST=OFF
+if errorlevel 1 exit 1
+
+cmake --build _build --target install
+if errorlevel 1 exit 1
