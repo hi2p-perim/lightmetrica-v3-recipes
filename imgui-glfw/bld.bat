5c2f02fa230d3858b27aa5a1f4c822edb22b48b0
Hisanari Otsu <hi2p.perim@gmail.com>
2019-05-14 19:08:22 +0200

    added imgui-glfw

---
+++ imgui-glfw/bld.bat
@@ -0,0 +1,17 @@
+cmake -H. -B_build ^
+      -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+cmake --build _build --target install
+if errorlevel 1 exit 1
+
+cmake -H. -B_build ^
+      -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Debug ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+cmake --build _build --target install
+if errorlevel 1 exit 1
