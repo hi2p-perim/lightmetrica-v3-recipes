6386bb629c1a868dd3fccbe01ebbf4d6475c77f6
Hisanari Otsu <hi2p.perim@gmail.com>
2019-05-16 09:09:07 +0200

    fixed recipe for lightmetrica

--- lightmetrica/bld.bat
+++ lightmetrica/bld.bat
@@ -1,7 +1,7 @@
 cmake -H. -B_build ^
-      -G "NMake Makefiles" ^
+      -G "Ninja" ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D LM_BUILD_TESTS=OFF ^
       -D LM_BUILD_EXAMPLES=OFF
 if errorlevel 1 exit 1
@@ -10,9 +10,9 @@ cmake --build _build --target install
 if errorlevel 1 exit 1
 
 cmake -H. -B_build ^
-      -G "NMake Makefiles" ^
+      -G "Ninja" ^
       -D CMAKE_BUILD_TYPE=Debug ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D LM_BUILD_TESTS=OFF ^
       -D LM_BUILD_EXAMPLES=OFF
 if errorlevel 1 exit 1
