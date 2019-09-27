bda15e5b011252dfc965c28573e308c9f738d9a9
Hisanari Otsu <hi2p.perim@gmail.com>
2019-09-27 16:56:48 +0200

    added vdbloader

---
+++ vdbloader/bld.bat
@@ -0,0 +1,9 @@
+cmake -H. -B_build ^
+      -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CONDA_PREFIX=%PREFIX%
+if errorlevel 1 exit 1
+
+cmake --build _build --target install
+if errorlevel 1 exit 1
---
+++ vdbloader/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cmake -H. -B_build \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CONDA_PREFIX=$PREFIX
+
+cmake --build _build --target install
